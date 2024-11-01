c0397d202056aabb50543b08161d9fd83750801b
ericKwon95 <ericyong95@gmail.com>
2024-11-01 10:37:06 +0900

    feat: Initial Settings

---
+++ SwiftUI_Library/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Library
+//
+//  Created by 권승용 on 11/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Library/SwiftUI_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LibraryApp.swift
+//  SwiftUI_Library
+//
+//  Created by 권승용 on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
